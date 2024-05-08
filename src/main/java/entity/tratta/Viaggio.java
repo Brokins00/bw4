1c19b60ec54eb8147a722a38282214342cc08435
Massimiliano Dalla Mura <dalla.mura91@gmail.com>
2024-05-08 14:43:22 +0200

    tratta e viaggio

--- src/main/java/entity/tratta/Viaggio.java
+++ src/main/java/entity/tratta/Viaggio.java
@@ -1,10 +1,60 @@
 package entity.tratta;
 
+import javax.persistence.*;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name="viaggi")
 public class Viaggio {
+    @Column(name="orario_partenza")
     private LocalDateTime orarioPartenza;
+    @Column(name="orario_arrivo")
     private LocalDateTime orarioArrivo;
+    @Column(name="percorrenza_effettiva")
     private Duration percorrenzaEffettiva;
+    @ManyToOne
+    @JoinColumn(name = "tratta_id")
+    private Tratta tratta;
+
+
+
+    public Viaggio() {
+
+    }
+
+    public LocalDateTime getOrarioPartenza() {
+        return orarioPartenza;
+    }
+
+    public void setOrarioPartenza(LocalDateTime orarioPartenza) {
+        this.orarioPartenza = orarioPartenza;
+    }
+
+    public LocalDateTime getOrarioArrivo() {
+        return orarioArrivo;
+    }
+
+    public void setOrarioArrivo(LocalDateTime orarioArrivo) {
+        this.orarioArrivo = orarioArrivo;
+    }
+
+    public Duration getPercorrenzaEffettiva() {
+        return percorrenzaEffettiva;
+    }
+
+    public void setPercorrenzaEffettiva(Duration percorrenzaEffettiva) {
+        this.percorrenzaEffettiva = percorrenzaEffettiva;
+    }
+
+    @Override
+    public String toString() {
+        return "Viaggio{" +
+                "orarioPartenza=" + orarioPartenza +
+                ", orarioArrivo=" + orarioArrivo +
+                ", percorrenzaEffettiva=" + percorrenzaEffettiva +
+                '}';
+    }
 }
+
+
